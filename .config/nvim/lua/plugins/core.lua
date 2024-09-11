a0dd71322e6a3c4fc6fb39df9379ea5a8e1413fc
waifod <waifod@proton.me>
2024-09-11 23:54:44 +0200

    Fix nvim-tmux integration

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -129,14 +129,6 @@ return {
     config = function()
       require("nvim-tmux-navigation").setup({
         disable_when_zoomed = true, -- defaults to false
-        keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
-          last_active = "<C-\\>",
-          next = "<C-Space>",
-        },
       })
     end,
   },
