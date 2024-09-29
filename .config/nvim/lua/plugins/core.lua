c130659bf4546f9017be9c305f4c3d619a1d16d4
waifod <waifod@pm.me>
2024-09-29 22:51:28 +0200

    Add tmux-resurrect

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -132,4 +132,18 @@ return {
       })
     end,
   },
+
+  --- Restore sessions
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  },
 }
