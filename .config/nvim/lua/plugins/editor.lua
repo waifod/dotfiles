cb2687407d5f8d731ae3422d73c6e5e5b3cc88ab
Matteo Durante <dumatte@amazon.com>
2025-08-10 23:21:24 +0200

    Fix Neovim-Tmux integration

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -12,24 +12,13 @@ return {
     },
   },
 
-  -- Add the tmux navigator plugin.
+  -- Add the tmux navigation plugin.
   {
     "alexghergh/nvim-tmux-navigation",
-    -- This plugin is configured via its `config` function.
     config = function()
       require("nvim-tmux-navigation").setup({
-        -- Disable navigation when a tmux pane is zoomed.
-        disable_when_zoomed = true,
+        disable_when_zoomed = true, -- defaults to false
       })
-      -- Set the keymaps for tmux navigation.
-      -- These are set here because the plugin needs to be loaded first.
-      -- The conflicting default keymaps have already been removed in `lua/config/keymaps.lua`.
-      local nvim_tmux_nav = require("nvim-tmux-navigation")
-      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-      vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
     end,
   },
 }
